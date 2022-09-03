193887ad1b177fa1538224ffa8e17cdc438de593
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-09-03 21:31:11 +0200

    Add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router"
 import '@/index.css'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: "/",
+        component: () => import("@/views/IndexPage.vue"),
+        name: 'index',
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: 'default',
+        redirect: "/"
+    } ,
+] as RouteRecordRaw[]
+
+const router = createRouter({
+    scrollBehavior(to) {
+        if (to.hash) {
+            return {
+                el: to.hash,
+            }
+        }
+    },
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router)
+    .mount('#app')
