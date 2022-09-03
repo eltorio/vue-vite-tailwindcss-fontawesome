f03346c53991b33bd2e4dd6950b19ab38c745439
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-09-03 18:21:00 +0200

    initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,31 @@
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const purgefonts = require('postcss-purgefonts')
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    purgecss({
+      content: ['./index.html', './src/**/*.ts', './src/**/*.js', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+      },
+      safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+      fontFace: true
+    }),
+    // purgefonts({
+    //   'purge_only_fonts': [
+    //     'Font Awesome 5 Free',
+    //     'Font Awesome 5 Brands',
+    //     'Font Awesome 6 Free',
+    //     'Font Awesome 6 Brands',
+    //     'Font Awesome 6 Duotone',
+    //     'Font Awesome 5 Brands',
+    //     'Font Awesome 5 Duotone',
+    //     'FontAwesome']
+    // })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    <i class="fab fa-linux"></i> Template Vue3 + Vite.js + TailwindCss + FontAwesome! <i class="fas fa-parachute-box"></i>
+  </h1>
+</template>
+<script  lang="ts">
+
+</script>
