8727f5b0b293269aca6fa861891ed656c16b93dc
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-10-09 12:07:46 +0200

    update template

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,28 @@ This website use:
 - Vite, Vue3, FontAwesome 6, TailwindCss 3
 - And many others
 */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import vitePluginFontawesomeminify from './fontawesome'
+import { defineConfig } from "vite";
+import fs from "fs";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import vitePluginFontawesomeminify from "./fontawesome";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vitePluginFontawesomeminify()
-  ],
+  plugins: [vue(), vitePluginFontawesomeminify()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~': path.resolve(__dirname, './node_modules'),
-      '§': path.resolve(__dirname, './'),
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "./node_modules"),
+      "§": path.resolve(__dirname, "./"),
     },
-  }
-})
+  },
+  server: {
+    https: fs.existsSync("./localhost.key")
+      ? {
+          key: fs.readFileSync("./localhost.key"),
+          cert: fs.readFileSync("./localhost.pem"),
+        }
+      : false,
+  },
+});
